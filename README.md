7ab663f7cb9aa6f89be87c56e3e1723a1ad9560a
Chito <Javier.martinez632@gmail.com>
2022-10-25 14:44:47 -0700

    updated JS

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 
 ## credit 
 https://www.youtube.com/watch?v=VwxGKpvW8Zk
+
+https://www.ceos3c.com/javascript/store-user-input-in-a-variable-with-javascript/
--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,27 @@
 var searchBtn = $("#btn");
+var dayBlk = $("#day-block");
 var APIKey = "2165f11d31c7c574376d30da15a79ef6";
-var city = $("#userInput").value;
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+var weatherCity = $("#userinput");
 
-console.log(city);
+
 function getCityWeather() {
+    var queryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + weatherCity + "&limit=1&appid=" + APIKey
 
 fetch(queryURL)
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
+        console.log(data);
         for (var i = 0; i < data.length; i++) {
-        console.log(data)
-        var cityTemp = $(".city-info");
+        var cityTemp = $("<h3>");
+        var tempList = $("<p>");
+        cityTemp.text(data[i]);
+        tempList.text(data[i]);
+    
+        cityTemp.append(tempList);
+        dayBlk.append(cityTemp);
 
-        cityTemp.textContent = data[i].html_url;
         }
     });
 }
