852fc47a2a1313642fdec5ab3db3ac42b95bcc46
Chito <Javier.martinez632@gmail.com>
2022-10-25 21:57:42 -0700

    updated js, url changed

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,31 @@
 var searchBtn = $("#btn");
 var dayBlk = $("#day-block");
 var APIKey = "2165f11d31c7c574376d30da15a79ef6";
-var weatherCity = $("#userinput");
+var weatherCity = $("#userinput").val();
 
 
 function getCityWeather() {
-    var queryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + weatherCity + "&limit=1&appid=" + APIKey
-
-fetch(queryURL)
-    .then(function (response) {
-        return response.json();
+    var queryURL = "http://api.openweathermap.org/geo/1.0/direct?q={city name}&appid=" + APIKey;
+    console.log("hello world");
+    fetch(queryURL)
+     .then(function (response) {
+         return response.json();
     })
     .then(function (data) {
         console.log(data);
         for (var i = 0; i < data.length; i++) {
-        var cityTemp = $("<h3>");
-        var tempList = $("<p>");
-        cityTemp.text(data[i]);
-        tempList.text(data[i]);
+        var cityName = $("<h3>");
+        var weatherVal = $("<p>")
+        cityName.text(data[i].name);
+        weatherVal.text(data[i].list.weather)
     
-        cityTemp.append(tempList);
-        dayBlk.append(cityTemp);
+        cityName.append(weatherVal);
+        dayBlk.append(cityName);
 
         }
     });
+    
 }
 
 searchBtn.on("click", getCityWeather);
+
